3e11b3d78988252ff0f510a7a0365ee6c501a39f
Robert Lupas <robertlupas09@gmail.com>
2024-09-24 18:03:19 +0300

    Update Visualizer.java

--- Visualizer/src/main/java/com/primetechrobotics/visualizer/Visualizer.java
+++ Visualizer/src/main/java/com/primetechrobotics/visualizer/Visualizer.java
@@ -9,26 +9,30 @@ public class Visualizer {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity robot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(160, 160, Math.toRadians(360), Math.toRadians(360), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
+        robot.runAction(robot.getDrive().actionBuilder(new Pose2d(60, -30, 0))
+                .lineToX(50)
                 .turn(Math.toRadians(90))
-                .lineToY(30)
+                .lineToY(50)
                 .turn(Math.toRadians(90))
-                .lineToX(0)
+                .lineToX(-55)
                 .turn(Math.toRadians(90))
-                .lineToY(0)
+                .lineToY(-55)
                 .turn(Math.toRadians(90))
+                .lineToX(60)
+                .turn(Math.toRadians(90))
+                .lineToY(-30)
+                .turn(Math.toRadians(-90))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(robot)
                 .start();
     }
 }
