09d8507a2a0f74f131f1a074a918d992ec405bed
Robert Lupas <robertlupas09@gmail.com>
2024-10-20 20:10:38 +0300

    Implement claw functionality
    Open+Closed & Front+Back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ro025/Outtake/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ro025/Outtake/Claw.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.ro025.Outtake;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.ro025.Gamepad.GamepadClass;
 
@@ -12,7 +13,28 @@ public class Claw {
     Servo rotationServo = null;
     Servo frontBackServo = null;
 
-    public static synchronized Claw getInstance() { //creezi o instanta
+    private final ElapsedTime actionTimer = new ElapsedTime();
+
+    enum OpenState {
+        OPEN, CLOSED
+    }
+
+    OpenState openState = OpenState.OPEN;
+
+    enum FrontBackState {
+        FRONT, BACK
+    }
+
+    FrontBackState frontBackState = FrontBackState.FRONT;
+
+    //Servo positions
+    //TODO: adjust with actual values
+    final double OPEN_POS = 1.0;
+    final double CLOSED_POS = 0.0;
+    final double FRONT_POS = 1.0;
+    final double BACK_POS = 0.0;
+
+    public static synchronized Claw getInstance() {
         if (instance == null) {
             instance = new Claw();
         }
@@ -21,18 +43,47 @@ public class Claw {
 
     public void init() {
         openingServo = hardwareMap.get(Servo.class, "openingServo");
-
         rotationServo = hardwareMap.get(Servo.class, "rotationServo");
-
         frontBackServo = hardwareMap.get(Servo.class, "frontBackServo");
+
+        actionTimer.reset();
     }
 
     public void loop() {
-        if (GamepadClass.getInstance().triangle()) {
-            //FSM intre inchis si deschis
+        // Opening/closing FSM
+        switch (openState) {
+            case OPEN:
+                if (GamepadClass.getInstance().triangle()) {
+                    // Transition to CLOSED state
+                    openingServo.setPosition(CLOSED_POS);
+                    openState = OpenState.CLOSED;
+                }
+                break;
+            case CLOSED:
+                if (GamepadClass.getInstance().triangle()) {
+                    // Transition to OPEN state
+                    openingServo.setPosition(OPEN_POS);
+                    openState = OpenState.OPEN;
+                }
+                break;
         }
-        if (GamepadClass.getInstance().square()) {
-            ///FSM intre fata spate
+
+        // Front/back movement FSM
+        switch (frontBackState) {
+            case FRONT:
+                if (GamepadClass.getInstance().square()) {
+                    // Transition to BACK state
+                    frontBackServo.setPosition(BACK_POS);
+                    frontBackState = FrontBackState.BACK;
+                }
+                break;
+            case BACK:
+                if (GamepadClass.getInstance().square()) {
+                    // Transition to FRONT state
+                    frontBackServo.setPosition(FRONT_POS);
+                    frontBackState = FrontBackState.FRONT;
+                }
+                break;
         }
 
         //TODO trebuie rotatie (ne intelegem cum o facem)
