cbbd7552e4afd2d1973d0ed23e2cc75d0c17d2d8
matei135 <matei.chiorean135@gmail.com>
2024-11-09 11:17:17 +0200

    Safety for Hang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ro025/TeleOp/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ro025/TeleOp/TeleOp.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.ro025.DriveTrain.DriveTrain;
 import org.firstinspires.ftc.teamcode.ro025.Gamepad.GamepadClass;
 import org.firstinspires.ftc.teamcode.ro025.Hang.Hang;
+import org.firstinspires.ftc.teamcode.ro025.Hang.Safety;
 import org.firstinspires.ftc.teamcode.ro025.Outtake.OutTake;
 //ne mai trebuie ceva aici sa porneasca? ionkno
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp25")
@@ -23,7 +24,7 @@ public class TeleOp extends OpMode {
         DriveTrain.getInstance().loop();
         GamepadClass.getInstance().loop();
         OutTake.getInstance().loop();
-        Hang.getInstance().loop();
+        Safety.getInstance().loop();
     }
 
 }
